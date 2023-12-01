9fbd9c32319d566ed248a33dbaa7bc5b38697f06
Bagas Adi Prayitno <aprayitno084@gmail.com>
2023-12-01 16:18:45 +0700

    update

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -32,6 +32,17 @@ const Header = () => {
               </div>
             </a>
           </div>
+          <div className="menu_1 w-1/3">
+            <div>
+                <label className="relative block">
+                <span className="sr-only">Search</span>
+                <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+                    <svg className="h-5 w-5 fill-slate-300" viewBox="0 0 20 20">...</svg>
+                </span>
+                <input className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Search for anything..." type="text" name="search"/>
+                </label>
+            </div>
+          </div>
           <div className='burger p-2 inline-block'>
             <div className="line_1 w-8"></div>
             <div className="line_2 w-8 mt-2"></div>
--- src/pages/Profil.jsx
+++ src/pages/Profil.jsx
@@ -1,5 +1,5 @@
 // import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
+// import Banyuwangi_1 from '../assets/banyuwangi_1.png'
 // import viteLogo from '/vite.svg'
 // import './App.css'
 import Header from "./Header"
@@ -9,16 +9,27 @@ const Profil = () => {
   
     return (
       <>
-            <Header/>
-            <div className="content w-full justify-center">
-                <div className="content_1 inline-block justify-center">
-                    <div className='isi pt-16'>
-                    <h1 className='title text-6xl'>Hello Everyone!!</h1>
-                    {/* <h1 className='title text-4xl mt-5'>Banyuwangi</h1> */}
-                    <h1 className='text-2xl mt-5'>This is Bagas Adi Prayitno</h1>
-                    </div>
-                </div>
+        <Header/>
+        <div className="content w-full flex flex-col">
+          <div className="content_11 w-full">
+            <div className='isi pt-16'>
+              <h1 className='title text-6xl'>Hello Everyone!!</h1>
+              <h1 className='text-2xl mt-5'>This is Bagas Adi Prayitno</h1>
             </div>
+          </div>
+          <div className="content_12 w-full pt-6">
+            <div className='isi_1 pt-20 flex flex-col justify-center w-full'>
+              {/* <div className='flex justify-center'>
+                <img src={Banyuwangi_1} alt="" className='w-1/6'/>
+              </div> */}
+              <div className='flex justify-center'>
+                <h1>I am the one and only children in my house. An ordinary guy from Banyuwangi city who have a dream to become an extraordinary guy. Obsessed on Information Technology, especially in software Development. Currently working at Secretary Ministry of Indonesia as a Fullstack Web Developer</h1>
+              </div>
+            </div>
+            {/* <div className='isi_2 pt-20 w-1/2'>
+            </div> */}
+          </div>
+        </div>
       </>
     )
   }
