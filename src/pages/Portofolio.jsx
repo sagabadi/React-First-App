be2efc16fdc3460ab0480dfa20500fec8143bcc4
Bagas Adi Prayitno <aprayitno084@gmail.com>
2023-12-05 16:32:36 +0700

    update

--- src/pages/Portofolio.jsx
+++ src/pages/Portofolio.jsx
@@ -14,15 +14,7 @@ const Portofolio = () => {
         <div className="content w-full justify-center">
             <div className="content_1 inline-block justify-center">
                 <div className='isi pt-16'>
-                <h1 className='title text-6xl'>EST. 1998</h1>
-                {/* <h1 className='title text-4xl mt-5'>Banyuwangi</h1> */}
-                <h1 className='text-2xl mt-5'>Bagas Adi Prayitno</h1>
-                </div>
-            </div>
-            <div className="content_2 inline-block">
-                <div className='isi pt-16'>
-                <h1 className='title text-6xl'>EST. 1998</h1>
-                <h1 className='text-2xl mt-5'>Bagas Adi Prayitno</h1>
+                <h1 className='title text-6xl'>Portofolio</h1>
                 </div>
             </div>
         </div>
